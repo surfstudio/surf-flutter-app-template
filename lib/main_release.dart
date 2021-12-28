ec7c278dd2edaaccb92206757c7a45c2282b13f5
KristinaZoteva <kristina1994misha@gmail.com>
2021-12-28 12:55:49 +0300

    Reorganaized imports.

--- lib/main_release.dart
+++ lib/main_release.dart
@@ -1,6 +1,6 @@
-import 'package:flutter_template/config/environment/build_types.dart';
 import 'package:flutter_template/config/app_config.dart';
 import 'package:flutter_template/config/debug_options.dart';
+import 'package:flutter_template/config/environment/build_types.dart';
 import 'package:flutter_template/config/environment/environment.dart';
 import 'package:flutter_template/config/urls.dart';
 import 'package:flutter_template/runner.dart';
