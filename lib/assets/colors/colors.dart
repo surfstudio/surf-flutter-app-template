ee56557f959e5086ddaf487260478bcf73ed3d4f
Кристина Зотьева <kristina1994misha@gmail.com>
2022-03-24 16:38:38 +0400

    Refactor: rename _grey color to _black54.

--- lib/assets/colors/colors.dart
+++ lib/assets/colors/colors.dart
@@ -15,7 +15,7 @@ const Color white = _white;
 const Color dividerColor = _solitude;
 const Color textColorSecondary = _darkGrey;
 const Color textColorPrimary = _oxfordBlue;
-const Color textColorGrey = _grey;
+const Color textColorGrey = _black54;
 
 // Main palette (http://www.color-blindness.com/color-name-hue/).
 const Color _oxfordBlue = Color(0xFF263238);
@@ -26,4 +26,4 @@ const Color _white = Colors.white;
 const Color _pastelWhite = Color(0xFFFAFAFA);
 const Color _solitude = Color(0xFFE5E8EB);
 const Color _darkGrey = Color(0xFFA7A7A7);
-const Color _grey = Color(0x8A000000);
+const Color _black54 = Colors.black54;
