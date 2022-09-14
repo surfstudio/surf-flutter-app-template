6be72cfbdb80a1312e77936223dba653647ca082
Elena <kulikova.vlg@gmail.com>
2022-09-14 11:59:01 +0300

    SNP-21: update temp routes for show temp screen first

--- lib/features/navigation/domain/entity/temp/temp_routes.dart
+++ lib/features/navigation/domain/entity/temp/temp_routes.dart
@@ -14,6 +14,7 @@ const tempRoutes = AutoRoute<dynamic>(
   children: [
     AutoRoute<dynamic>(
       page: TempScreen,
+      path: '',
     ),
     debugRoutes,
   ],
--- lib/features/navigation/service/router.gr.dart
+++ lib/features/navigation/service/router.gr.dart
@@ -18,29 +18,45 @@ class _$AppRouter extends RootStackRouter {
   @override
   final Map<String, PageFactory> pagesMap = {
     HomeRoute.name: (routeData) {
-      return MaterialPageX<dynamic>(routeData: routeData, child: const EmptyRouterPage());
+      return MaterialPageX<dynamic>(
+          routeData: routeData, child: const EmptyRouterPage());
     },
     TempRouter.name: (routeData) {
-      return MaterialPageX<dynamic>(routeData: routeData, child: const EmptyRouterPage());
+      return MaterialPageX<dynamic>(
+          routeData: routeData, child: const EmptyRouterPage());
     },
     TempRoute.name: (routeData) {
-      final args = routeData.argsAs<TempRouteArgs>(orElse: () => const TempRouteArgs());
-      return MaterialPageX<dynamic>(routeData: routeData, child: TempScreen(key: args.key, wmFactory: args.wmFactory));
+      final args =
+          routeData.argsAs<TempRouteArgs>(orElse: () => const TempRouteArgs());
+      return MaterialPageX<dynamic>(
+          routeData: routeData,
+          child: TempScreen(key: args.key, wmFactory: args.wmFactory));
     },
     DebugRouter.name: (routeData) {
-      final args = routeData.argsAs<DebugRouterArgs>(orElse: () => const DebugRouterArgs());
-      return MaterialPageX<dynamic>(routeData: routeData, child: DebugScreen(key: args.key, wmFactory: args.wmFactory));
+      final args = routeData.argsAs<DebugRouterArgs>(
+          orElse: () => const DebugRouterArgs());
+      return MaterialPageX<dynamic>(
+          routeData: routeData,
+          child: DebugScreen(key: args.key, wmFactory: args.wmFactory));
     }
   };
 
   @override
   List<RouteConfig> get routes => [
         RouteConfig(HomeRoute.name, path: '/', children: [
-          RouteConfig('#redirect', path: '', parent: HomeRoute.name, redirectTo: 'temp', fullMatch: true),
-          RouteConfig(TempRouter.name, path: 'temp', parent: HomeRoute.name, children: [
-            RouteConfig(TempRoute.name, path: 'temp-screen', parent: TempRouter.name),
-            RouteConfig(DebugRouter.name, path: 'debug', parent: TempRouter.name)
-          ])
+          RouteConfig('#redirect',
+              path: '',
+              parent: HomeRoute.name,
+              redirectTo: 'temp',
+              fullMatch: true),
+          RouteConfig(TempRouter.name,
+              path: 'temp',
+              parent: HomeRoute.name,
+              children: [
+                RouteConfig(TempRoute.name, path: '', parent: TempRouter.name),
+                RouteConfig(DebugRouter.name,
+                    path: 'debug', parent: TempRouter.name)
+              ])
         ])
       ];
 }
@@ -48,7 +64,8 @@ class _$AppRouter extends RootStackRouter {
 /// generated route for
 /// [EmptyRouterPage]
 class HomeRoute extends PageRouteInfo<void> {
-  const HomeRoute({List<PageRouteInfo>? children}) : super(HomeRoute.name, path: '/', initialChildren: children);
+  const HomeRoute({List<PageRouteInfo>? children})
+      : super(HomeRoute.name, path: '/', initialChildren: children);
 
   static const String name = 'HomeRoute';
 }
@@ -56,7 +73,8 @@ class HomeRoute extends PageRouteInfo<void> {
 /// generated route for
 /// [EmptyRouterPage]
 class TempRouter extends PageRouteInfo<void> {
-  const TempRouter({List<PageRouteInfo>? children}) : super(TempRouter.name, path: 'temp', initialChildren: children);
+  const TempRouter({List<PageRouteInfo>? children})
+      : super(TempRouter.name, path: 'temp', initialChildren: children);
 
   static const String name = 'TempRouter';
 }
@@ -66,19 +84,23 @@ class TempRouter extends PageRouteInfo<void> {
 class TempRoute extends PageRouteInfo<TempRouteArgs> {
   TempRoute(
       {Key? key,
-      WidgetModel<ElementaryWidget<IWidgetModel>, ElementaryModel> Function(BuildContext) wmFactory =
-          initScreenWidgetModelFactory})
-      : super(TempRoute.name, path: 'temp-screen', args: TempRouteArgs(key: key, wmFactory: wmFactory));
+      WidgetModel<ElementaryWidget<IWidgetModel>, ElementaryModel> Function(
+              BuildContext)
+          wmFactory = initScreenWidgetModelFactory})
+      : super(TempRoute.name,
+            path: '', args: TempRouteArgs(key: key, wmFactory: wmFactory));
 
   static const String name = 'TempRoute';
 }
 
 class TempRouteArgs {
-  const TempRouteArgs({this.key, this.wmFactory = initScreenWidgetModelFactory});
+  const TempRouteArgs(
+      {this.key, this.wmFactory = initScreenWidgetModelFactory});
 
   final Key? key;
 
-  final WidgetModel<ElementaryWidget<IWidgetModel>, ElementaryModel> Function(BuildContext) wmFactory;
+  final WidgetModel<ElementaryWidget<IWidgetModel>, ElementaryModel> Function(
+      BuildContext) wmFactory;
 
   @override
   String toString() {
@@ -91,19 +113,24 @@ class TempRouteArgs {
 class DebugRouter extends PageRouteInfo<DebugRouterArgs> {
   DebugRouter(
       {Key? key,
-      WidgetModel<ElementaryWidget<IWidgetModel>, ElementaryModel> Function(BuildContext) wmFactory =
-          debugScreenWidgetModelFactory})
-      : super(DebugRouter.name, path: 'debug', args: DebugRouterArgs(key: key, wmFactory: wmFactory));
+      WidgetModel<ElementaryWidget<IWidgetModel>, ElementaryModel> Function(
+              BuildContext)
+          wmFactory = debugScreenWidgetModelFactory})
+      : super(DebugRouter.name,
+            path: 'debug',
+            args: DebugRouterArgs(key: key, wmFactory: wmFactory));
 
   static const String name = 'DebugRouter';
 }
 
 class DebugRouterArgs {
-  const DebugRouterArgs({this.key, this.wmFactory = debugScreenWidgetModelFactory});
+  const DebugRouterArgs(
+      {this.key, this.wmFactory = debugScreenWidgetModelFactory});
 
   final Key? key;
 
-  final WidgetModel<ElementaryWidget<IWidgetModel>, ElementaryModel> Function(BuildContext) wmFactory;
+  final WidgetModel<ElementaryWidget<IWidgetModel>, ElementaryModel> Function(
+      BuildContext) wmFactory;
 
   @override
   String toString() {
