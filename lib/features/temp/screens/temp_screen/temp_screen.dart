539256929b73350c917fb3baf18bde3d36353a5c
Elena <kulikova.vlg@gmail.com>
2022-09-15 10:27:43 +0300

    SNP-23: separate screens for debug and release mode, add debug screen for showing in debug mode

--- lib/features/temp/screens/temp_screen/temp_screen.dart
+++ lib/features/temp/screens/temp_screen/temp_screen.dart
@@ -1,3 +1,4 @@
+import 'package:auto_route/auto_route.dart';
 import 'package:elementary/elementary.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_template/features/temp/screens/temp_screen/temp_screen_widget_model.dart';
@@ -12,6 +13,55 @@ class TempScreen extends ElementaryWidget<TempScreenWidgetModel> {
 
   @override
   Widget build(TempScreenWidgetModel wm) {
-    return const Scaffold();
+    return AutoTabsRouter.tabBar(
+      routes: wm.routes,
+      builder: (context, child, controller) {
+        final tabsRouter = context.tabsRouter;
+        return Scaffold(
+          appBar: AppBar(
+            title: Text(wm.appBarTitle(context.topRoute)),
+          ),
+          body: child,
+          bottomNavigationBar: wm.isDebugMode
+              ? const _SingleBottomNavBarItem()
+              : BottomNavigationBar(
+                  currentIndex: tabsRouter.activeIndex,
+                  onTap: tabsRouter.setActiveIndex,
+                  items: wm.navigationBarItems,
+                ),
+        );
+      },
+    );
+  }
+}
+
+class _SingleBottomNavBarItem extends StatelessWidget {
+  const _SingleBottomNavBarItem({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      elevation: 8.0,
+      child: SafeArea(
+        child: Container(
+          padding: const EdgeInsets.symmetric(vertical: 8.0),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Icon(
+                Icons.bug_report_outlined,
+                color: Theme.of(context).primaryColor,
+              ),
+              Text(
+                'Debug screen',
+                style: TextStyle(color: Theme.of(context).primaryColor),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
--- lib/features/temp/screens/temp_screen/temp_screen_widget_model.dart
+++ lib/features/temp/screens/temp_screen/temp_screen_widget_model.dart
@@ -1,5 +1,10 @@
+import 'package:auto_route/auto_route.dart';
 import 'package:elementary/elementary.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_template/config/app_config.dart';
+import 'package:flutter_template/config/environment/environment.dart';
+import 'package:flutter_template/features/navigation/domain/entity/app_route_paths.dart';
+import 'package:flutter_template/features/navigation/service/router.dart';
 import 'package:flutter_template/features/temp/screens/temp_screen/temp_screen.dart';
 import 'package:flutter_template/features/temp/screens/temp_screen/temp_screen_model.dart';
 
@@ -7,15 +12,70 @@ import 'package:flutter_template/features/temp/screens/temp_screen/temp_screen_m
 TempScreenWidgetModel initScreenWidgetModelFactory(
   BuildContext context,
 ) {
-  final model = TempScreenModel();
+  final model = TempScreenModel(Environment<AppConfig>.instance());
   return TempScreenWidgetModel(model);
 }
 
 /// Widget model for [TempScreen].
-class TempScreenWidgetModel extends WidgetModel<TempScreen, TempScreenModel> implements IDebugWidgetModel {
+class TempScreenWidgetModel extends WidgetModel<TempScreen, ITempScreenModel>
+    implements IDebugWidgetModel {
+  @override
+  bool get isDebugMode => model.isDebugMode;
+
+  @override
+  List<PageRouteInfo> get routes => _routes;
+
+  @override
+  List<BottomNavigationBarItem> get navigationBarItems => _navigationBarItems;
+
+  List<PageRouteInfo> get _routes {
+    return isDebugMode ? [DebugRouter()] : [DashRouter(), InfoRouter()];
+  }
+
+  List<BottomNavigationBarItem> get _navigationBarItems {
+    return [
+      const BottomNavigationBarItem(
+        label: 'Dash screen',
+        icon: Icon(Icons.flutter_dash),
+      ),
+      const BottomNavigationBarItem(
+        label: 'Info screen',
+        icon: Icon(Icons.info_outline),
+      ),
+    ];
+  }
+
   /// Create an instance [TempScreenWidgetModel].
-  TempScreenWidgetModel(TempScreenModel model) : super(model);
+  TempScreenWidgetModel(ITempScreenModel model) : super(model);
+
+  @override
+  String appBarTitle(RouteData topRoute) => _appBarTitle(topRoute);
+
+  String _appBarTitle(RouteData topRoute) {
+    switch (topRoute.path) {
+      case AppRoutePaths.debugPath:
+        return 'Экран отладки';
+      case AppRoutePaths.dashPath:
+        return 'Dash';
+      case AppRoutePaths.infoPath:
+        return 'Info';
+      default:
+        return '';
+    }
+  }
 }
 
 /// Interface of [TempScreenWidgetModel].
-abstract class IDebugWidgetModel extends IWidgetModel {}
+abstract class IDebugWidgetModel extends IWidgetModel {
+  /// Return value for app environment status.
+  bool get isDebugMode;
+
+  /// Routes for [AutoTabsRouter.tabBar].
+  List<PageRouteInfo<dynamic>> get routes;
+
+  /// Items for [BottomNavigationBar].
+  List<BottomNavigationBarItem> get navigationBarItems;
+
+  /// Title for appbar, depends on current selected page.
+  String appBarTitle(RouteData topRoute);
+}
