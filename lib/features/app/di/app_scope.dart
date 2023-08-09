ff32cb2e9520e5832796e6fb234436cfe9f812ad
Tatiana Sugina <internetova@yandex.ru>
2023-08-09 15:12:45 +0300

    SNP-660 (feat): updated AppScope

--- lib/features/app/di/app_scope.dart
+++ lib/features/app/di/app_scope.dart
@@ -2,10 +2,18 @@ import 'package:dio/dio.dart';
 import 'package:dio/io.dart';
 import 'package:elementary/elementary.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:flutter_template/config/environment/environment.dart';
+import 'package:flutter_template/features/common/service/interceptors/auth_token_interceptor.dart';
+import 'package:flutter_template/features/common/service/interceptors/refresh_token_interceptor.dart';
+import 'package:flutter_template/features/common/service/interceptors/retry_interceptor.dart';
+import 'package:flutter_template/features/common/service/repository/refresh_token/refresh_token_impl.dart';
+import 'package:flutter_template/features/common/service/repository/refresh_token/refresh_token_repository.dart';
 import 'package:flutter_template/features/common/service/theme/theme_service.dart';
 import 'package:flutter_template/features/common/service/theme/theme_service_impl.dart';
 import 'package:flutter_template/features/navigation/service/router.dart';
+import 'package:flutter_template/persistence/storage/auth_tokens/auth_tokens_storage.dart';
+import 'package:flutter_template/persistence/storage/auth_tokens/auth_tokens_storage_secure.dart';
 import 'package:flutter_template/persistence/storage/theme_storage/theme_storage.dart';
 import 'package:flutter_template/persistence/storage/theme_storage/theme_storage_impl.dart';
 import 'package:flutter_template/util/default_error_handler.dart';
@@ -21,6 +29,12 @@ class AppScope implements IAppScope {
 
   @override
   late VoidCallback applicationRebuilder;
+
+  @override
+  final FlutterSecureStorage secureStorage = const FlutterSecureStorage();
+
+  @override
+  late final IAuthTokensStorage authTokensStorage = _createAuthTokensStorage();
 
   @override
   Dio get dio => _dio;
@@ -39,7 +53,14 @@ class AppScope implements IAppScope {
   /// Create an instance [AppScope].
   AppScope() {
     /// List interceptor. Fill in as needed.
-    final additionalInterceptors = <Interceptor>[];
+    final additionalInterceptors = <Interceptor>[
+      AuthTokenInterceptor(authTokensStorage),
+      RefreshTokenInterceptor(
+        refreshTokenRepository,
+        dio,
+      ),
+      RetryInterceptor(dio),
+    ];
 
     _dio = _initDio(additionalInterceptors);
     _errorHandler = DefaultErrorHandler();
@@ -49,11 +70,14 @@ class AppScope implements IAppScope {
 
   @override
   Future<void> initTheme() async {
-    final theme =
-        await ThemeModeStorageImpl().getThemeMode() ?? _themeByDefault;
+    final theme = await ThemeModeStorageImpl().getThemeMode() ?? _themeByDefault;
     _themeService = ThemeServiceImpl(theme);
     _themeService.addListener(_onThemeModeChanged);
   }
+
+  @override
+  late final IRefreshTokenRepository refreshTokenRepository =
+      RefreshTokenRepositoryImpl(authTokensStorage);
 
   Dio _initDio(Iterable<Interceptor> additionalInterceptors) {
     const timeout = Duration(seconds: 30);
@@ -66,8 +90,7 @@ class AppScope implements IAppScope {
       ..receiveTimeout = timeout
       ..sendTimeout = timeout;
 
-    (dio.httpClientAdapter as IOHttpClientAdapter).onHttpClientCreate =
-        (client) {
+    (dio.httpClientAdapter as IOHttpClientAdapter).onHttpClientCreate = (client) {
       final proxyUrl = Environment.instance().config.proxyUrl;
       if (proxyUrl != null && proxyUrl.isNotEmpty) {
         client
@@ -85,8 +108,10 @@ class AppScope implements IAppScope {
     dio.interceptors.addAll(additionalInterceptors);
 
     if (Environment.instance().isDebug) {
-      dio.interceptors
-          .add(LogInterceptor(requestBody: true, responseBody: true));
+      dio.interceptors.add(LogInterceptor(
+        requestBody: true,
+        responseBody: true,
+      ));
     }
 
     return dio;
@@ -94,6 +119,10 @@ class AppScope implements IAppScope {
 
   Future<void> _onThemeModeChanged() async {
     await _themeModeStorage.saveThemeMode(mode: _themeService.currentThemeMode);
+  }
+
+  IAuthTokensStorage _createAuthTokensStorage() {
+    return AuthTokensStorageSecure(secureStorage);
   }
 }
 
@@ -116,4 +145,17 @@ abstract class IAppScope {
 
   /// Init theme service with theme from storage or default value.
   Future<void> initTheme();
+
+  // --- Repository ---
+
+  /// Repository for token refresh operation.
+  IRefreshTokenRepository get refreshTokenRepository;
+
+  // --- Storage ---
+
+  /// Storing data in secure storage
+  FlutterSecureStorage get secureStorage;
+
+  /// Storage for authentication tokens of the application
+  IAuthTokensStorage get authTokensStorage;
 }
