63cc24c9b2ca01dd8801305bccf0e0c2fff0b407
Tatiana Sugina <internetova@yandex.ru>
2023-08-31 12:02:46 +0300

    (feat): updated clean_ios.sh

--- scripts/clean_ios.sh
+++ scripts/clean_ios.sh
@@ -1,10 +1,13 @@
-#!/usr/bin/environment bash
-flutter clean
-cd ios/
-pod cache clean --all
-#rm -rf ~/Library/Developer/Xcode/DerivedData todo uncomment
-xcodebuild clean
-rm -rf .symlinks/
-rm -rf Pods
-rm -rf Podfile.lock
-cd ..
+#!/usr/bin/env bash
+
+if [ "$(basename "$PWD")" = 'scripts' ]; then cd ..; fi
+
+fvm flutter clean
+
+(cd ios/ &&
+  pod cache clean --all &&
+  #rm -rf ~/Library/Developer/Xcode/DerivedData &&
+  xcodebuild clean &&
+  rm -rf .symlinks/ &&
+  rm -rf Pods &&
+  rm -rf Podfile.lock)
