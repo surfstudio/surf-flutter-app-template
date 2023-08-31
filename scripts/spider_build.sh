9d7ee4a6b28748ac47685ba22377dbab2058a182
Tatiana Sugina <internetova@yandex.ru>
2023-08-31 12:15:16 +0300

    (feat): updated spider_build.sh

--- scripts/spider_build.sh
+++ scripts/spider_build.sh
@@ -1,4 +1,6 @@
-if [ "$(basename "$PWD")" = 'script' ]; then cd ..; fi
+#!/usr/bin/env bash
+
+if [ "$(basename "$PWD")" = 'scripts' ]; then cd ..; fi
 
 fvm flutter pub run spider build &&
   fvm dart format . && fvm flutter test --update-goldens test/assets/images_test.dart
