2ec00d348a835564a4f219a184196cf16fe4949f
Evgenia-bit <ev.mo4alina@yandex.ru>
2024-01-25 14:38:38 +0300

    feat: updated hooks

--- tools/bricks/screen/hooks/post_gen.dart
+++ tools/bricks/screen/hooks/post_gen.dart
@@ -3,35 +3,9 @@ import 'dart:io';
 import 'package:mason/mason.dart';
 
 Future<void> run(HookContext context) async {
-  await makeFormat();
-  await makeDcm();
-  await makeAnalyze();
-}
-
-Future<void> makeFormat() async {
   final process = await Process.start(
     'fvm',
     ['dart', 'format', '-l', '100', 'lib', 'test'],
-  );
-
-  await process.stdout.pipe(stdout);
-  await process.stderr.pipe(stderr);
-}
-
-Future<void> makeDcm() async {
-  final process = await Process.start(
-    'dcm',
-    ['analyze', 'lib', 'test'],
-  );
-
-  await process.stdout.pipe(stdout);
-  await process.stderr.pipe(stderr);
-}
-
-Future<void> makeAnalyze() async {
-  final process = await Process.start(
-    'fvm',
-    ['flutter', 'analyze', 'lib', 'test'],
   );
 
   await process.stdout.pipe(stdout);
