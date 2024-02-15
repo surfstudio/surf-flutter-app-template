9a79acb6f3fa61a2e831aa65d5f5e6fd82a51a41
DmitrDomrachev <domrachev@surf.dev>
2024-02-15 14:39:18 +0300

    refactor: navigation structure

--- lib/features/navigation/domain/entity/info/info_routes.dart
+++
@@ -1,9 +0,0 @@
-import 'package:auto_route/auto_route.dart';
-import 'package:flutter_template/features/navigation/domain/entity/app_route_paths.dart';
-import 'package:flutter_template/features/navigation/service/router.dart';
-
-/// All routes for the info feature.
-final infoRoutes = AutoRoute(
-  page: InfoRouter.page,
-  path: AppRoutePaths.infoPath,
-);
--- lib/features/navigation/domain/entity/log_history/log_history_routes.dart
+++
@@ -1,9 +0,0 @@
-import 'package:auto_route/auto_route.dart';
-import 'package:flutter_template/features/navigation/domain/entity/app_route_paths.dart';
-import 'package:flutter_template/features/navigation/service/router.dart';
-
-/// All routes for the history logs feature.
-final logHistoryRoutes = AutoRoute(
-  page: LogHistoryRouter.page,
-  path: AppRoutePaths.logHistory,
-);
