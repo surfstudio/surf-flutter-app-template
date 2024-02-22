2dd62f2619cf9ac75a9aaf8cf153ca8004ddff39
Mochalina Evgeniya <63193897+Evgenia-bit@users.noreply.github.com>
2024-02-22 09:37:43 +0300

    feat: Updated analytics version (#74)
    * feat: updated analytics version
    
    * feat: updated file names
    
    * chore: AnalyticService in app scope replaced to AnalyticActionPerformer
    
    * refactor: removed amplitude analitycs

--- lib/features/dash/presentation/dash_wm.dart
+++ lib/features/dash/presentation/dash_wm.dart
@@ -1,8 +1,8 @@
+import 'package:analytics/core/analytyc_service.dart';
 import 'package:elementary/elementary.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter_template/common/utils/analytics/event/common/track_analytics_example.dart';
-import 'package:flutter_template/common/utils/analytics/service/analytics_service.dart';
 import 'package:flutter_template/features/app/di/app_scope.dart';
 import 'package:flutter_template/features/common/utils/mixin/theme_mixin.dart';
 import 'package:flutter_template/features/dash/presentation/dash_model.dart';
@@ -24,10 +24,8 @@ DashWidgetModel dashScreenWmFactory(
 }
 
 /// Widget model for [DashScreen].
-class DashWidgetModel extends WidgetModel<DashScreen, DashModel>
-    with ThemeWMMixin
-    implements IDashWidgetModel {
-  final IAnalyticsService _analyticsService;
+class DashWidgetModel extends WidgetModel<DashScreen, DashModel> with ThemeWMMixin implements IDashWidgetModel {
+  final AnalyticService _analyticsService;
 
   @override
   AppLocalizations get l10n => context.l10n;
@@ -35,20 +33,18 @@ class DashWidgetModel extends WidgetModel<DashScreen, DashModel>
   /// Create an instance [DashWidgetModel].
   DashWidgetModel({
     required DashModel model,
-    required IAnalyticsService analyticsService,
+    required AnalyticService analyticsService,
   })  : _analyticsService = analyticsService,
         super(model);
 
   @override
   void trackAnalyticsExample() {
-    _analyticsService.trackEvent(const TrackAnalyticsExampleEvent());
+    _analyticsService.performAction(const TrackAnalyticsExampleEvent());
   }
 }
 
 /// Interface for [DashWidgetModel].
-abstract class IDashWidgetModel
-    with ThemeIModelMixin
-    implements IWidgetModel {
+abstract class IDashWidgetModel with ThemeIModelMixin implements IWidgetModel {
   /// Localization strings.
   AppLocalizations get l10n;
 
