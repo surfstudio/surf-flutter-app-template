2bf355e1ea7b3b131bb52c22ceb195e353fcfd03
Tatiana Sugina <internetova@yandex.ru>
2024-02-22 19:15:01 +0300

    (feat): dcm InactiveWrapper test

--- test/uikit/others/inactive_wrapper_test.dart
+++ test/uikit/others/inactive_wrapper_test.dart
@@ -3,6 +3,10 @@ import 'package:flutter/material.dart';
 import 'package:flutter_template/uikit/others/inactive_wrapper.dart';
 import 'package:golden_toolkit/golden_toolkit.dart';
 
+const _double100 = 100.0;
+const _double200 = 200.0;
+const _double350 = 350.0;
+
 void main() {
   testGoldens(
     'Golden Inactive wrapper',
@@ -13,27 +17,28 @@ void main() {
         ..addScenario(
           'Inactive black square',
           InactiveWrapper(
-            child: Container(
-              height: 100,
-              width: 100,
-              color: Colors.black,
-            ),
             inactive: true,
+            child: Container(
+              color: Colors.black,
+              width: _double100,
+              height: _double100,
+            ),
           ),
         )
         ..addScenario(
           'Active black square',
           InactiveWrapper(
-            child: Container(
-              height: 100,
-              width: 100,
-              color: Colors.black,
-            ),
             inactive: false,
+            child: Container(
+              color: Colors.black,
+              width: _double100,
+              height: _double100,
+            ),
           ),
         );
 
-      await tester.pumpWidgetBuilder(builder.build(), surfaceSize: const Size(200, 350));
+      await tester.pumpWidgetBuilder(builder.build(),
+          surfaceSize: const Size(_double200, _double350));
 
       await screenMatchesGolden(tester, 'incative_wrapper');
     },
