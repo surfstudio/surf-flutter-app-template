226c3aafd64bbe02c40dd0a5a46cac6f626c5efc
Tatiana Sugina <internetova@yandex.ru>
2024-02-22 18:46:28 +0300

    (feat): dcm add AppSizes

---
+++ lib/features/common/utils/sizes/app_sizes.dart
@@ -0,0 +1,10 @@
+/// Frequent sizes.
+abstract class AppSizes {
+
+  /// Value double 8.
+  static const double8 = 8.0;
+  /// Value double 16.
+  static const double16 = 16.0;
+  /// Value double 20.
+  static const double20 = 20.0;
+}
