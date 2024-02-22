8cabc5d75e44bd49ae04fd4f7fc0b172ef411687
Tatiana Sugina <internetova@yandex.ru>
2024-02-22 19:06:59 +0300

    (feat): dcm DiScope

--- lib/common/widgets/di_scope/di_scope.dart
+++ lib/common/widgets/di_scope/di_scope.dart
@@ -7,46 +7,46 @@ typedef ScopeFactory<T> = T Function();
 
 /// Di container. T - return type(for example [AppScope]).
 class DiScope<T> extends StatefulWidget {
+  /// Create an instance [DiScope].
+  const DiScope({
+    required this.onFactory,
+    required this.child,
+    this.onDispose,
+    super.key,
+  });
+
   /// Factory that returns the dependency scope.
-  final ScopeFactory<T> factory;
+  final ScopeFactory<T> onFactory;
 
   /// The widget below this widget in the tree.
   final Widget child;
 
   /// The method called when disposing the widget.
-  final Function(T)? dispose;
-
-  /// Create an instance [DiScope].
-  const DiScope({
-    required this.factory,
-    required this.child,
-    this.dispose,
-    super.key,
-  });
+  final ValueChanged<T>? onDispose;
 
   @override
   State<DiScope<T>> createState() => _DiScopeState<T>();
 }
 
 class _DiScopeState<T> extends State<DiScope<T>> {
-  late T scope;
+  late T _scope;
 
   @override
   void initState() {
     super.initState();
-    scope = widget.factory();
+    _scope = widget.onFactory();
   }
 
   @override
   void dispose() {
-    widget.dispose?.call(scope);
+    widget.onDispose?.call(_scope);
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     return Provider<T>(
-      create: (_) => scope,
+      create: (_) => _scope,
       child: widget.child,
     );
   }
