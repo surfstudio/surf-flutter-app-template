a629e34fc1a8e6fc5471f72f99778dba626da104
DmitrDomrachev <domrachev@surf.dev>
2024-02-22 14:14:42 +0300

    refactor: router structure changed

--- lib/features/navigation/domain/entity/dash/dash_routes.dart
+++
@@ -1,9 +0,0 @@
-import 'package:auto_route/auto_route.dart';
-import 'package:flutter_template/features/navigation/domain/entity/app_route_paths.dart';
-import 'package:flutter_template/features/navigation/service/router.dart';
-
-/// All routes for the dash feature.
-final dashRoutes = AutoRoute(
-  page: DashRouter.page,
-  path: AppRoutePaths.dashPath,
-);
