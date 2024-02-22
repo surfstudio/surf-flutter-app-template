2b7a9320bcae33063baf72d79f73aad841abb20f
Tatiana Sugina <internetova@yandex.ru>
2024-02-22 19:02:56 +0300

    (feat): dcm DefaultErrorHandler

--- lib/common/utils/default_error_handler.dart
+++ lib/common/utils/default_error_handler.dart
@@ -1,8 +1,13 @@
 import 'package:elementary/elementary.dart';
 
+/// {@template default_error_handler.class}
 /// Base implementation of error handler.
 /// This just print error to console.
+/// {@endtemplate}
 class DefaultErrorHandler implements ErrorHandler {
+  /// {@macro default_error_handler.class}
+  const DefaultErrorHandler();
+
   @override
   void handleError(Object error, {StackTrace? stackTrace}) {
     // ignore: avoid_print
