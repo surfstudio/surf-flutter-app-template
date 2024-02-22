a3203b6b8a82ac4869cab806ef971687d1e64f15
Tatiana Sugina <internetova@yandex.ru>
2024-02-22 19:00:22 +0300

    (feat): dcm DebugRepository

--- lib/features/debug/data/repositories/debug_repository.dart
+++ lib/features/debug/data/repositories/debug_repository.dart
@@ -1,7 +1,7 @@
 import 'package:flutter_template/features/debug/domain/repositories/i_debug_repository.dart';
 
 /// {@template debug_repository.class}
-/// Implementation of Debug repository
+/// Implementation of Debug repository.
 /// {@endtemplate}
 final class DebugRepository implements IDebugRepository {
   /// {@macro debug_repository.class}
