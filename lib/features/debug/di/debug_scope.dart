b85d78f0a9d26c3936b3adabb7e8d6814d2be967
Tatiana Sugina <internetova@yandex.ru>
2024-02-22 19:01:38 +0300

    (feat): dcm DebugScope

--- lib/features/debug/di/debug_scope.dart
+++ lib/features/debug/di/debug_scope.dart
@@ -3,12 +3,6 @@ import 'package:flutter_template/common/utils/disposable_object/i_disposable_obj
 import 'package:flutter_template/features/debug/data/repositories/debug_repository.dart';
 import 'package:flutter_template/features/debug/domain/repositories/i_debug_repository.dart';
 
-/// Interface for Debug DI Scope.
-abstract interface class IDebugScope implements IDisposableObject {
-  /// Debug repository
-  abstract final IDebugRepository debugRepository;
-}
-
 /// {@template debug_scope.class}
 /// Dependencies scope of Debug module.
 /// {@endtemplate}
@@ -16,11 +10,17 @@ final class DebugScope extends DisposableObject implements IDebugScope {
   @override
   final IDebugRepository debugRepository;
 
-  /// factory constructor for [DebugScope]
+  /// Factory constructor for [DebugScope].
   factory DebugScope.create() {
     return DebugScope(const DebugRepository());
   }
 
   /// {@macro debug_scope.class}
   DebugScope(this.debugRepository);
+}
+
+/// Interface for Debug DI Scope.
+abstract interface class IDebugScope implements IDisposableObject {
+  /// Debug repository.
+  abstract final IDebugRepository debugRepository;
 }
