f60c38f60b3c4f71640ea26aac6ecfd4e4495381
Tatiana Sugina <internetova@yandex.ru>
2024-02-23 16:23:30 +0300

    (feat): dcm InactiveWrapper

--- lib/uikit/others/inactive_wrapper.dart
+++ lib/uikit/others/inactive_wrapper.dart
@@ -1,24 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_template/features/common/utils/sizes/app_sizes.dart';
 
 /// {@template inactive_wrapper.class}
 /// A wrapper for displaying inactive elements.
 /// {@endtemplate}
 class InactiveWrapper extends StatelessWidget {
-  /// @nodoc
+  /// {@macro inactive_wrapper.class}
+  const InactiveWrapper(
+      {required this.child, required this.inactive, super.key});
+
+  /// @nodoc.
   final Widget child;
 
   /// Whether the widget is inactive.
   final bool inactive;
-
-  /// {@macro inactive_wrapper.class}
-  const InactiveWrapper({required this.child, required this.inactive, super.key});
 
   @override
   Widget build(BuildContext context) {
     return IgnorePointer(
       ignoring: inactive,
       child: Opacity(
-        opacity: inactive ? 0.5 : 1,
+        opacity: inactive ? AppSizes.double05 : AppSizes.double1,
         child: child,
       ),
     );
