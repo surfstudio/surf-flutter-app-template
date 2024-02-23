9311ef4487e0fddda52f7eb9788c6e23fedefddc
Tatiana Sugina <internetova@yandex.ru>
2024-02-23 16:41:51 +0300

    (feat): dcm FirebaseAnalyticStrategy

--- lib/common/utils/analytics/firebase/firebase_analytic_strategy.dart
+++ lib/common/utils/analytics/firebase/firebase_analytic_strategy.dart
@@ -3,7 +3,7 @@ import 'package:flutter_template/common/utils/analytics/base/analytic_event.dart
 import 'package:flutter_template/common/utils/analytics/mock/mock_firebase_analytics.dart';
 
 /// {@template firebase_analytic_strategy.class}
-/// A class for sending analytics data to Firebase
+/// A class for sending analytics data to Firebase.
 /// {@endtemplate}
 class FirebaseAnalyticStrategy extends AnalyticStrategy<AnalyticEvent> {
   /// The instance of MockFirebaseAnalytics used for sending analytics data.
