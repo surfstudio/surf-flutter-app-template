bfe2c88751f64539ccfb89cc4c5805306c4133be
Tatiana Sugina <internetova@yandex.ru>
2024-02-23 16:41:18 +0300

    (feat): dcm Dash

--- lib/features/dash/data/repositories/dash_repository.dart
+++ lib/features/dash/data/repositories/dash_repository.dart
@@ -1,7 +1,7 @@
 import 'package:flutter_template/features/dash/domain/repositories/i_dash_repository.dart';
 
 /// {@template dash_repository.class}
-/// Implementation of Dash repository
+/// Implementation of Dash repository.
 /// {@endtemplate}
 final class DashRepository implements IDashRepository {
   /// {@macro dash_repository.class}
--- lib/features/dash/di/dash_scope.dart
+++ lib/features/dash/di/dash_scope.dart
@@ -3,12 +3,6 @@ import 'package:flutter_template/common/utils/disposable_object/i_disposable_obj
 import 'package:flutter_template/features/dash/data/repositories/dash_repository.dart';
 import 'package:flutter_template/features/dash/domain/repositories/i_dash_repository.dart';
 
-/// Interface for Dash DI Scope.
-abstract interface class IDashScope implements IDisposableObject {
-  /// Dash repository
-  abstract final IDashRepository dashRepository;
-}
-
 /// {@template dash_scope.class}
 /// Dependencies scope of Dash module.
 /// {@endtemplate}
@@ -16,11 +10,17 @@ final class DashScope extends DisposableObject implements IDashScope {
   @override
   final IDashRepository dashRepository;
 
-  /// Factory constructor for [DashScope]
+  /// Factory constructor for [DashScope].
   factory DashScope.create() {
     return DashScope(const DashRepository());
   }
 
   /// {@macro dash_scope.class}
   DashScope(this.dashRepository);
+}
+
+/// Interface for Dash DI Scope.
+abstract interface class IDashScope implements IDisposableObject {
+  /// Dash repository.
+  abstract final IDashRepository dashRepository;
 }
