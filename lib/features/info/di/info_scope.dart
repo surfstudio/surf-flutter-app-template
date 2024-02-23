0db1a0d859c77b19dd0071cca8e9329c52556434
Tatiana Sugina <internetova@yandex.ru>
2024-02-23 16:44:09 +0300

    (feat): dcm comments

--- lib/features/info/di/info_scope.dart
+++ lib/features/info/di/info_scope.dart
@@ -3,12 +3,6 @@ import 'package:flutter_template/common/utils/disposable_object/i_disposable_obj
 import 'package:flutter_template/features/info/data/repositories/info_repository.dart';
 import 'package:flutter_template/features/info/domain/repositories/i_info_repository.dart';
 
-/// Interface for Info DI Scope.
-abstract interface class IInfoScope implements IDisposableObject {
-  /// Info repository
-  abstract final IInfoRepository infoRepository;
-}
-
 /// {@template info_scope.class}
 /// Dependencies scope of Info module.
 /// {@endtemplate}
@@ -16,11 +10,17 @@ final class InfoScope extends DisposableObject implements IInfoScope {
   @override
   final IInfoRepository infoRepository;
 
-  /// Factory constructor for [InfoScope]
+  /// Factory constructor for [InfoScope].
   factory InfoScope.create() {
     return InfoScope(const InfoRepository());
   }
 
   /// {@macro info_scope.class}
   InfoScope(this.infoRepository);
+}
+
+/// Interface for Info DI Scope.
+abstract interface class IInfoScope implements IDisposableObject {
+  /// Info repository.
+  abstract final IInfoRepository infoRepository;
 }
