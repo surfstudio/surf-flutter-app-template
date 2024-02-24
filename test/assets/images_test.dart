f45632ef14065ce8757ebcfcd4276f770d292ece
Tatiana Sugina <internetova@yandex.ru>
2024-02-24 17:09:54 +0300

    (feat): dcm Images golden test

--- test/assets/images_test.dart
+++ test/assets/images_test.dart
@@ -3,6 +3,9 @@ import 'package:flutter/widgets.dart';
 import 'package:flutter_template/assets/resources.dart';
 import 'package:golden_toolkit/golden_toolkit.dart';
 
+const _double300 = 300.0;
+const _double500 = 500.0;
+
 void main() {
   testGoldens('Images golden test', (tester) async {
     final builder = GoldenBuilder.column(
@@ -18,8 +21,8 @@ void main() {
 
     await tester.pumpWidgetBuilder(
       builder.build(),
-      surfaceSize: Size(500, Images.values.length * 300),
       wrapper: materialAppWrapper(),
+      surfaceSize: Size(_double500, Images.values.length * _double300),
     );
 
     await screenMatchesGolden(tester, 'images');
