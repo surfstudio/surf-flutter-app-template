9dd0c7605fd581075ea847f1f1542da4ccd455ec
Tatiana Sugina <internetova@yandex.ru>
2024-02-24 17:34:31 +0300

    (feat): dcm TempRepository

--- lib/features/temp/data/repositories/temp_repository.dart
+++ lib/features/temp/data/repositories/temp_repository.dart
@@ -1,7 +1,7 @@
 import 'package:flutter_template/features/temp/domain/repositories/i_temp_repository.dart';
 
 /// {@template temp_repository.class}
-/// Implementation of Template repository
+/// Implementation of Template repository.
 /// {@endtemplate}
 final class TempRepository implements ITempRepository {
   /// {@macro temp_repository.class}
