764299784a8faf2f090d5320e7071d93954af8d5
Evgenia-bit <ev.mo4alina@yandex.ru>
2024-03-04 15:32:23 +0300

    chore: cleanup

--- lib/persistence/storage/tokens_storage/tokens_storage.dart
+++ lib/persistence/storage/tokens_storage/tokens_storage.dart
@@ -1,53 +1,17 @@
-import 'package:flutter_secure_storage/flutter_secure_storage.dart';
-import 'package:flutter_template/persistence/storage/tokens_storage/i_tokens_storage.dart';
+/// Storage for tokens
+abstract interface class ITokensStorage {
+  /// Get accessToken
+  Future<String?> accessToken();
 
-/// {@template tokens_storage.class}
-/// Implementation [ITokensStorage]
-/// {@endtemplate}
-final class TokensStorage implements ITokensStorage {
-  final FlutterSecureStorage _secureStorage;
+  /// Save accessToken
+  Future<void> saveAccessToken(String accessToken);
 
-  /// {@macro tokens_storage.class}
-  const TokensStorage(this._secureStorage);
+  /// Get refreshToken
+  Future<String?> refreshToken();
 
-  @override
-  Future<String?> accessToken() {
-    return _secureStorage.read(key: TokensStorageKeys.accessToken.keyName);
-  }
+  /// Save refreshToken
+  Future<void> saveRefreshToken(String refreshToken);
 
-  @override
-  Future<void> saveAccessToken(String accessToken) async {
-    await _secureStorage.write(key: TokensStorageKeys.accessToken.keyName, value: accessToken);
-  }
-
-  @override
-  Future<String?> refreshToken() {
-    return _secureStorage.read(key: TokensStorageKeys.refreshToken.keyName);
-  }
-
-  @override
-  Future<void> saveRefreshToken(String refreshToken) async {
-    await _secureStorage.write(key: TokensStorageKeys.refreshToken.keyName, value: refreshToken);
-  }
-
-  @override
-  Future<void> clear() async {
-    for (final key in TokensStorageKeys.values) {
-      await _secureStorage.delete(key: key.keyName);
-    }
-  }
-}
-
-/// Keys for [TokensStorage]
-enum TokensStorageKeys {
-  /// @nodoc
-  accessToken('app_access_token'),
-
-  /// @nodoc
-  refreshToken('app_refresh_token');
-
-  /// Key name
-  final String keyName;
-
-  const TokensStorageKeys(this.keyName);
+  /// Clear storage
+  Future<void> clear();
 }
---
+++ lib/persistence/storage/tokens_storage/tokens_storage_impl.dart
@@ -0,0 +1,53 @@
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:flutter_template/persistence/storage/tokens_storage/tokens_storage.dart';
+
+/// {@template tokens_storage.class}
+/// Implementation [ITokensStorage]
+/// {@endtemplate}
+final class TokensStorageImpl implements ITokensStorage {
+  final FlutterSecureStorage _secureStorage;
+
+  /// {@macro tokens_storage.class}
+  const TokensStorageImpl(this._secureStorage);
+
+  @override
+  Future<String?> accessToken() {
+    return _secureStorage.read(key: TokensStorageKeys.accessToken.keyName);
+  }
+
+  @override
+  Future<void> saveAccessToken(String accessToken) async {
+    await _secureStorage.write(key: TokensStorageKeys.accessToken.keyName, value: accessToken);
+  }
+
+  @override
+  Future<String?> refreshToken() {
+    return _secureStorage.read(key: TokensStorageKeys.refreshToken.keyName);
+  }
+
+  @override
+  Future<void> saveRefreshToken(String refreshToken) async {
+    await _secureStorage.write(key: TokensStorageKeys.refreshToken.keyName, value: refreshToken);
+  }
+
+  @override
+  Future<void> clear() async {
+    for (final key in TokensStorageKeys.values) {
+      await _secureStorage.delete(key: key.keyName);
+    }
+  }
+}
+
+/// Keys for [TokensStorageImpl]
+enum TokensStorageKeys {
+  /// @nodoc
+  accessToken('app_access_token'),
+
+  /// @nodoc
+  refreshToken('app_refresh_token');
+
+  /// Key name
+  final String keyName;
+
+  const TokensStorageKeys(this.keyName);
+}
