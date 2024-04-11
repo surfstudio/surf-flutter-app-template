99bdbce78a3352f666b203b338e8202a28111978
Tatiana Sugina <internetova@yandex.ru>
2024-04-11 15:06:19 +0300

    (feat): add entities

---
+++ lib/features/snack_queue/domain/entities/snack_message_type.dart
@@ -0,0 +1,11 @@
+/// The type of message on the snack bar.
+enum SnackMessageType {
+  /// Successfully completed.
+  success,
+
+  /// Error.
+  error,
+
+  /// Warning.
+  warning,
+}
---
+++ lib/features/snack_queue/domain/entities/top_snack_bar.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_template/features/snack_queue/domain/entities/snack_message_type.dart';
+
+/// Display information for the snack.
+@immutable
+class TopSnackBar {
+  /// Message in the snack.
+  final String message;
+
+  /// Type of message.
+  final SnackMessageType messageType;
+
+  /// Snack display time.
+  final DateTime showTime;
+
+  /// Create an instance [TopSnackBar].
+  TopSnackBar({
+    required this.message,
+    required this.messageType,
+  }) : showTime = DateTime.now();
+
+  @override
+  String toString() {
+    return 'TopSnackBar{messageType: $messageType, message: $message';
+  }
+}
